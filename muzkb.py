55d85c4cd334e73ce69755c8e29bfa7b8073eac8
Yotam Medini <yotam.medini@n2ws.com>
2019-01-02 18:17:10 +0200

    muzkb - still just playing with Cairo

--- muzkb.py
+++ muzkb.py
@@ -14,6 +14,34 @@ import cairo
 def elog(msg):
     sys.stderr.write('%s\n' % msg)
 
+
+def draw(da, cr):
+    elog('draw(da=%s, cr=%s)' % (da, cr))
+    w = da.get_allocated_width()
+    h = da.get_allocated_height()
+    size = min(w,h)
+
+    cr.set_source_rgb(0.0,0.2,0.0)
+    cr.paint()
+
+    cr.set_source_rgb(1.0,0.0,0.0)
+    
+    cr.arc(0.5*w,0.5*h,0.5*size,0.0,6.3)
+    cr.fill()
+
+
+win = Gtk.Window(title="Music Keyboard")
+win.connect("destroy", Gtk.main_quit)
+vbox = Gtk.VBox()
+
+drawingarea = Gtk.DrawingArea()
+vbox.add(drawingarea)
+drawingarea.connect('draw', draw)
+win.add(vbox)
+win.show_all()
+Gtk.main()
+
+
 if __name__ == '__main__':
     elog('Hello')
     elog('Bye')
