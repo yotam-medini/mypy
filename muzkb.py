755249c2cae47a825972d03cca9960810c610cd1
Yotam Medini <yotam.medini@n2ws.com>
2019-01-09 14:29:07 +0200

    muzkb: Locator allocate & print - work ok

--- muzkb.py
+++ muzkb.py
@@ -14,12 +14,16 @@ import cairo
 def elog(msg):
     sys.stderr.write('%s\n' % msg)
 
+def fatal(msg):
+    elog('Fatal %s' % msg)
+    sys.exit(1)
+
 class Segment:
     def __init__(self, xb, xe):
         self.xb = xb
         self.xe = xe
     def __str__(self):
-        return '[%d,%e)' % (self.xb, self.xe)
+        return '[%d, %d)' % (self.xb, self.xe)
 
 class Locator:
 
@@ -27,10 +31,10 @@ class Locator:
     KEY_UNDEFINED = -2
 
     class Key:
-        def __init__(self, note, inner_segment, outer_segment):
+        def __init__(self, note, inner, outer):
             self.note = note
-            self.inner_segment = inner_segment
-            self.outer_segment = outer_segment
+            self.inner = inner
+            self.outer = outer
         def __str__(self):
             return '{Key(%d, inner=%s, outer=%s}' % (self.note, self.inner, self.outer)
 
@@ -54,22 +58,15 @@ class Locator:
         return not self.note_is_white(note)
 
     def allocate(self):
-        # Consider width white-key = 1
+        # Consider (front) width white-key = 1
         # b = black key width
         # w1 = white-back-width C,D,E
         # w2 = white-back-width F,G,A,B
-        #   3w1 + 2b = 3
-        #   4w2 + 3b = 4
-        # ==>
-        #   w1 = -2b/3 + 1
-        #   w2 = -3b/4 + 1
-        #   w1 - b = -5b/3 + 1
-        #   w2 - b = -7b/4 + 1
-        self.allocate_white()
-        self.allocate_black()
+        #   3w1 + 2b = 3  ==>   w1 = -2b/3 + 1
+        #   4w2 + 3b = 4  ==>   w2 = -3b/4 + 1
+        #   Assuming b > 0, since 3/4 > 2/3, so w1 > w2. So we will requre  (w1 - b) = (b - w2)
+        #   -5b/3 + 1 = 7b/4 - 1   ==>  (7/4 + 5/3)b = 2   ==>  b = 24/41
 
-    def allocate_white(self):
-        self.white_keys = []
         self.low_white = self.note_low
         self.high_white = self.note_low
         if self.note_is_black(self.low_white):
@@ -77,32 +74,61 @@ class Locator:
         if self.note_is_black(self.high_white):
             self.high_white += -1
         n_whites = self.high_white = self.low_white + 1
-        self.white_outer_width = outer_width = self.w // n_whites
-        space = max(outer_width // 32, 1)
-        # inner_width = outer_width - 2*space
-        wi = 0
-        for note in (self.low_white, self.high_white + 1):
-            if self.note_is_white(note):
-                xl = (wi*self.w + n_whites//2) // n_whites
-                xr = xl + outer_width
-                self.white_keys.append(Locator.Key(note, Segment(xl, xr), Segment(xl + space, xr - space)))
-    
-    def allocate_black(self):
+        self.white_outer_width = self.w // n_whites
+        self.black_outer_width = (24*self.white_outer_width + 20) // 41
+        white_space = max(self.white_outer_width // 32, 1)
+        black_space = max(self.black_outer_width // 32, 1)
+
+        self.keys = []
+        self.white_keys = []
         self.black_keys = []
-        outer_width = self.w // (self.note_high - self.note_low + 1)
-        space = max(outer_width // 32, 1)
-        if self.note_is_black(self.note_low):
-            # self.black_keys.append(Key(self.note_low), )
-            pass
+        for note in range(self.note_low, self.note_high + 1):
+            if self.note_is_white(note):
+                wi = len(self.white_keys)
+                xl = (wi * self.w + n_whites//2) // n_whites
+                xr = xl + self.white_outer_width
+                key = Locator.Key(note, Segment(xl + white_space, xr - white_space), Segment(xl, xr))
+                self.white_keys.append(key)
+            else:
+                if note == self.note_low:
+                    xr = self.black_outer_width // 2
+                    key = Locator.Key(note, Segment(0, xr - black_space), Segment(0, xr))
+                else:
+                    penta = note % 12
+                    pre_white = self.white_keys[-1].outer.xe
+                    q = {
+                        1: (2, 3), # C#=Db
+                        3: (1, 3), # D#=Ef
+                        6: (3, 4), # F#=Gb
+                        8: (1, 2), # G#=Ab
+                        10: (1, 4)  # A#=Bb
+                    }[penta]
+                    xl = pre_white - (q[0] * self.black_outer_width + (q[1]//2)) // q[1]
+                    xl_inner = xl + black_space
+                    xr = xl + self.black_outer_width
+                    xr_inner = xr - black_space
+                    if xr >= self.w:
+                        xr = xr_inner = self.w
+                    key = Locator.Key(note, Segment(xl_inner, xr_inner), Segment(xl, xr))
+                self.black_keys.append(key)
+            self.keys.append(key)
 
     def pick(self, x):
         return -1
+
+    def print(self, f=sys.stdout):
+        f.write('{ #(White)=%d, #(Black)=%d\n' % (len(self.white_keys), len(self.black_keys)))
+        for key in self.keys:
+            note = key.note
+            f.write('%s%s\n' % (('' if self.note_is_white(note) else '    '), key))
+        f.write('}\n')
 
 
 def locator_test(argv):
     nums = list(map(int, argv))
     locator = Locator(nums[0], nums[1], nums[2], nums[3])
     elog('locator= %s' % locator)
+    locator.print()
     for n in nums[4:]:
         elog('pick(%d) = %d' % (n, locator.pick(n)))
     return 0
